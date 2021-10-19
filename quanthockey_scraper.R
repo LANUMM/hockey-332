d12069b9ce613df832fe6ee71ee4b16a8090bdfa
Matthew Lanum <matthewlanum@gmail.com>
2021-10-18 20:44:14 -0400

    fix files

--- quanthockey_scraper.R
+++ quanthockey_scraper.R
@@ -1,7 +1,37 @@
+
+#Install packages before use. 
+#Install firefox
+
 require(rvest)
 require(RSelenium)
 require(httr)
 require(tidyverse)
+
+mainPage <- read_html("https://www.quanthockey.com/nhl/seasons/2020-21-nhl-players-stats.html")
+table <- mainPage %>% html_element("#AjaxRefresh") %>% html_table()
+
+myDF <- data.frame(table)
+myDF
+
+rD <- rsDriver(browser="firefox", port=4545L, verbose=F)
+remDr <- rD[["client"]]
+
+remDr$navigate("https://www.quanthockey.com/nhl/seasons/2020-21-nhl-players-stats.html")
+
+page <- 2
+
+remDr$navigate(paste("javascript:PaginateStats('Season','Players','2020-21','0','2020-21','reg','P','DESC','",page,"','NHL','en',true);", sep=""))
+
+html <- remDr$getPageSource()[[1]]
+  rD <- rsDriver(browser="firefox", port=4545L, verbose=F)
+remDr <- rD[["client"]]
+
+remDr$navigate("https://www.quanthockey.com/nhl/seasons/2020-21-nhl-players-stats.html")
+
+
+
+
+
 
 rD <- rsDriver(browser="firefox", port=4545L, verbose=F)
 remDr <- rD[["client"]]
@@ -17,4 +47,5 @@ maxPage <- 20
 for(page in 2:maxPage){
   #TODO: 
 }
+
 
